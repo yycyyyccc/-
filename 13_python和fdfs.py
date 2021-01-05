f36312a1bab0035f09b1736f72c84e97ee41bdfc
yycyyyccc <1050653794@qq.com>
2021-01-05 13:51:59 +0800

    安装完成

---
+++ 13_python和fdfs.py
@@ -0,0 +1,18 @@
+workon bj18_py3
+
+安装一个包fdfs client-py-master
+(bj18_py3) yyc@yyc-Lenovo-G50-45:~/bj18$ unzip fdfs_client-py-master.zip
+解压
+这里报错版本太低，直接下面命令
+
+pip install py3Fdfs
+Python 3.8.5 (default, Jul 28 2020, 12:59:40) 
+[GCC 9.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> from fdfs_client.client import Fdfs_client, get_tracker_conf
+>>> tracker_path = get_tracker_conf('/etc/fdfs/client.conf')
+>>> client = Fdfs_client(tracker_path)
+>>> ret = client.upload_by_filename('./test')
+>>> print(ret)
+{'Group name': b'group1', 'Remote file_id': b'group1/M00/00/00/wKgfnl_z4IuAO4FrAAAABr99wgM4277861', 'Status': 'Upload successed.', 'Local file name': './test', 'Uploaded size': '6B', 'Storage IP': b'192.168.31.158'}
+>>> 
